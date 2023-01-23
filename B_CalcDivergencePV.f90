3c291249479d6ce9d0bcb79ed88644f9634b64b0
Vlad200118 <64873890+Vlad200118@users.noreply.github.com>
2023-01-23 04:56:48 +0300

    Add files via upload

--- B_CalcDivergencePV.f90
+++ B_CalcDivergencePV.f90
@@ -1,7 +1,7 @@
 Subroutine B_CalcDivergancePV(NI,NJ,V,P,GradP,CellVolume,IFaceVector,JFaceVector,IFaceCenter, &
                             JFaceCenter,CellCenter, DivPV, SCHEME)
     Implicit none
-    Integer :: NI, NJ ,I, J, IFace, I_neighbor, J_neighbor
+    Integer :: NI, NJ ,I, J, IFace, I_neighbor, J_neighbor, Bound
     Real :: SCHEME
     Real :: LinearInterpolation, PFace, PLinearInterpolation, PFOU, PSOU, GFace
     Real, Dimension (2) :: VLinearInterpolation
@@ -18,30 +18,43 @@ Subroutine B_CalcDivergancePV(NI,NJ,V,P,GradP,CellVolume,IFaceVector,JFaceVector
     DO J =1, NJ-1
         DO I=1, NI-1
             DO IFace=1, 4 !Цикл по соседям
+            Bound = 0
                 select case (IFace)
                 case(1)!сосед снизу
                     I_neighbor = I
                     J_neighbor = J-1
                     FaceCenter(:) =  JFaceCenter(I,J,:)
                     FaceVector(:) = -JFaceVector(I,J,:)!минус из-за внешней нормали
+                    IF(J .EQ. 1) THEN
+                        Bound = 1
+                    END IF
 
                 case(2)!сосед сверху
                     I_neighbor = I
                     J_neighbor = J+1
                     FaceCenter(:) = JFaceCenter(I,J+1,:)
                     FaceVector(:) = JFaceVector(I,J+1,:)
+                    IF(J .EQ. (NJ-1)) THEN
+                        Bound = 1
+                    END IF
 
                 case(3)!сосед слева
                     I_neighbor = I-1
                     J_neighbor = J
                     FaceCenter(:) =  IFaceCenter(I,J,:)
                     FaceVector(:) = -IFaceVector(I,J,:)!минус из-за внешней нормали
+                    IF(I .EQ. 1) THEN
+                        Bound = 1
+                    END IF
 
                 case(4)!сосед справа
                     I_neighbor = I+1
                     J_neighbor = J
                     FaceCenter(:) = IFaceCenter(I+1,J,:)
                     FaceVector(:) = IFaceVector(I+1,J,:)
+                    IF(I .EQ. (NI-1) ) THEN
+                        Bound = 1
+                    END IF
 
                 end select
 
@@ -56,23 +69,32 @@ Subroutine B_CalcDivergancePV(NI,NJ,V,P,GradP,CellVolume,IFaceVector,JFaceVector
 
                     GFace = DOT_PRODUCT(VLinearInterpolation(:), FaceVector(:))
 
-                    IF (SCHEME .EQ. 2.0) then
-                        IF (GFace .GE. 0) then
+                    IF (SCHEME .EQ. 2.0) THEN
+                        IF (GFace .GE. 0) THEN
                             PFOU = P(I,J)
-                        else
-                            PFOU = P(I_neighbor,J_neighbor)
-                        end if
+                        ELSE
+                            if (Bound .EQ. 1) THEN
+                                PFOU = 2.0*P(I_neighbor,J_neighbor)-P(I,J)
+                            ELSE
+                                PFOU = P(I_neighbor,J_neighbor)
+                            END IF
+                        END IF
                         PFace = PFOU
-                    end if
+                    END IF
 
-                    IF (SCHEME .LE. 1.0) then
-                        IF (GFace .GE. 0) then
+                    IF (SCHEME .LE. 1.0) THEN
+                        IF (GFace .GE. 0) THEN
                             PSOU = P(I,J) + DOT_PRODUCT(GradP(I,J,:),FaceCenter(:) - CellCenter(I,J,:))
-                        else
+                        ELSE
                             PSOU = P(I_neighbor,J_neighbor) + DOT_PRODUCT(GradP(I_neighbor,J_neighbor,:),FaceCenter(:) - CellCenter(I_neighbor,J_neighbor,:))
-                        end if
+                        END IF
+
+                        if (Bound .EQ. 1) THEN
+                            PLinearInterpolation=P(I_neighbor,J_neighbor)
+                        END IF
+
                         PFace = (1.0-SCHEME)*PLinearInterpolation + SCHEME * PSOU
-                    end if
+                    END IF
 
 
                     DivPV(I,J) = DivPV(I,J) + PFace*GFace
